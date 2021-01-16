25517792a161c66c7165f5851895bae5b78d09d7
Elizabeth Lam <animew.cccl@gmail.com>
2021-01-16 18:47:55 -0500

    Create past week cards widget

--- lib/config/palette.dart
+++ lib/config/palette.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 class Palette {
   static const Color primaryColor = Color(0xFF473F97);
+  static const Color secondaryColor = Color(0xFFF6F2FF);
 }
---
+++ lib/widgets/past_week_cards.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:mood_tracker/config/palette.dart';
+
+class PastWeekCards extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 5.0),
+      height: MediaQuery.of(context).size.height * 0.17,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: <Widget>[
+          DailyCards('assets/Happy.png', '11', context),
+          DailyCards('assets/Happy.png', '12', context),
+          DailyCards('assets/Happy.png', '13', context),
+          DailyCards('assets/Happy.png', '14', context),
+          DailyCards('assets/Happy.png', '15', context),
+          DailyCards('assets/Happy.png', '16', context),
+          DailyCards('assets/Happy.png', '17', context),
+        ],
+      ),
+    );
+  }
+
+  Container DailyCards(String imageVal, String date, BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.23,
+      child: Card(
+        color: Palette.secondaryColor,
+        elevation: 0.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Wrap(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 8.0),
+                child: Text(
+                  date,
+                  style: const TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Image.asset(
+                imageVal,
+                width: MediaQuery.of(context).size.width * 0.15,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
