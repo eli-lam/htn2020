cb880ea13e488dcb12c941bbcd53e85d6c26343e
Elizabeth Lam <animew.cccl@gmail.com>
2021-01-16 17:42:46 -0500

    Make base homepage header

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mood_tracker/config/palette.dart';
+import 'package:mood_tracker/config/styles.dart';
+import 'package:mood_tracker/widgets/widgets.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -11,9 +13,13 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
+      appBar: CustomAppBar(),
       body: CustomScrollView(
         physics: ClampingScrollPhysics(),
-        slivers: <Widget>[],
+        slivers: <Widget>[
+          _buildHeader(screenHeight),
+          _buildPast7Days(screenHeight),
+        ],
       ),
     );
   }
@@ -21,13 +27,81 @@ class _HomeScreenState extends State<HomeScreen> {
   SliverToBoxAdapter _buildHeader(double screenHeight) {
     return SliverToBoxAdapter(
       child: Container(
-        padding: const EdgeInsets.all(20.0),
+        padding: const EdgeInsets.symmetric(
+          vertical: 32.0,
+          horizontal: 16.0,
+        ),
         decoration: BoxDecoration(
           color: Palette.primaryColor,
           borderRadius: BorderRadius.only(
             bottomLeft: Radius.circular(40.0),
             bottomRight: Radius.circular(40.0),
           ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Hi Amy,',
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 32.0,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            SizedBox(height: screenHeight * 0.01),
+            Text(
+              'How\'s it going?',
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+              ),
+            ),
+            SizedBox(height: screenHeight * 0.05),
+            Row(
+              children: <Widget>[
+                FlatButton(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 14.0,
+                    horizontal: 24.0,
+                  ),
+                  onPressed: () {},
+                  color: Colors.black54,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  child: Text(
+                    'Talk to the chatbot',
+                    style: Styles.buttonTextStyle,
+                  ),
+                  textColor: Colors.white,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  SliverToBoxAdapter _buildPast7Days(double screenHeight) {
+    return SliverToBoxAdapter(
+      child: Container(
+        padding: const EdgeInsets.all(20.0),
+        color: Colors.orange,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Past 7 days',
+              style: const TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ],
         ),
       ),
     );
