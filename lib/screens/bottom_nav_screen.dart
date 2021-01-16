93655347798723597f4b6638dc01ec772cc72d0c
Elizabeth Lam <animew.cccl@gmail.com>
2021-01-16 16:18:31 -0500

    Setup project architecture and add base navigation bar

---
+++ lib/screens/bottom_nav_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:mood_tracker/screens/screens.dart';
+
+class BottomNavScreen extends StatefulWidget {
+  @override
+  _BottomNavScreenState createState() => _BottomNavScreenState();
+}
+
+class _BottomNavScreenState extends State<BottomNavScreen> {
+  final List _screens = [
+    HomeScreen(),
+    Scaffold(),
+    Scaffold(),
+    Scaffold(),
+    Scaffold(),
+  ];
+
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.white,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        elevation: 0.0,
+        items: [
+          Icons.home_rounded,
+          Icons.calendar_today,
+          Icons.chat_bubble_outline,
+          Icons.article_outlined,
+          Icons.person_outline
+        ]
+            .asMap()
+            .map((key, value) => MapEntry(
+                  key,
+                  BottomNavigationBarItem(
+                    title: Text(''),
+                    icon: Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 6.0,
+                        horizontal: 16.0,
+                      ),
+                      decoration: BoxDecoration(
+                        color: _currentIndex == key
+                            ? Colors.blue[600]
+                            : Colors.transparent,
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: Icon(value),
+                    ),
+                  ),
+                ))
+            .values
+            .toList(),
+      ),
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:mood_tracker/config/palette.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: CustomScrollView(
+        physics: ClampingScrollPhysics(),
+        slivers: <Widget>[],
+      ),
+    );
+  }
+
+  SliverToBoxAdapter _buildHeader(double screenHeight) {
+    return SliverToBoxAdapter(
+      child: Container(
+        padding: const EdgeInsets.all(20.0),
+        decoration: BoxDecoration(
+          color: Palette.primaryColor,
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(40.0),
+            bottomRight: Radius.circular(40.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
