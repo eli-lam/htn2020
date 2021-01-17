01616b697b6dea156148d54c81af24b2f9b5a5ea
k-chew <kailey.chew@uwaterloo.ca>
2021-01-17 01:43:56 -0800

    Update UI of reports screen

--- lib/screens/report_screen.dart
+++ lib/screens/report_screen.dart
@@ -13,20 +13,25 @@ CollectionReference collectionReference =
 class _ReportScreenState extends State<ReportScreen> {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      // define a StreamBuilder of type QuerySnapshot
-      stream: collectionReference
-          .snapshots(), // use snapshots() function on a CollectionReference
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (!snapshot.hasData)
-          return new Center(child: new CircularProgressIndicator());
-        return new Center(
-            child: new Row(
-                children: snapshot.data.documents
-                    .map((doc) => new Text(doc['emotion'] +
-                        DateFormat.yMMMd().format(doc['date'].toDate())))
-                    .toList()));
-      },
-    );
+    return Scaffold(
+        appBar: AppBar(title: Text('Weekly Report')),
+        body: StreamBuilder<QuerySnapshot>(
+          // define a StreamBuilder of type QuerySnapshot
+          stream: collectionReference
+              .snapshots(), // use snapshots() function on a CollectionReference
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (!snapshot.hasData)
+              return new Center(child: new CircularProgressIndicator());
+            return new Center(
+                child: new Container(
+                    child: new Column(
+                        children: snapshot.data.documents
+                            .map((doc) => new Text(doc['emotion'] +
+                                DateFormat.yMMMd()
+                                    .format(doc['date'].toDate())))
+                            .toList())));
+          },
+        ));
   }
 }
